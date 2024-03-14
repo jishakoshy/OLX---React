bb00cec8d52b1957d49d1a7419c4b3d783e6ac1f
jishakoshy <135108986+jishakoshy@users.noreply.github.com>
2024-03-14 11:11:10 +0530

    moving to env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import 'firebase/storage'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAajtjqTLQN_1jRXyUbEip1X4KuJ_GjCGs",
-    authDomain: "reactjs-olx-37735.firebaseapp.com",
-    projectId: "reactjs-olx-37735",
-    storageBucket: "reactjs-olx-37735.appspot.com",
-    messagingSenderId: "526140758118",
-    appId: "1:526140758118:web:08dc0765f1d94c7b26b5a7",
-    measurementId: "G-YGQV16S8GV"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
   };
 
   export default firebase.initializeApp(firebaseConfig)
